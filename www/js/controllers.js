25055effa24fe022fd9e53c0739bfe6fba86b108
Roger Florat <rflorat07@gmail.com>
2016-02-24 00:47:20 +0100

    Creasión de la pantalla (Mappa de la Sede)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,20 +25,30 @@ angular.module('starter.controllers', [])
 	    var mapOptions = {
 	        center: latlng,
 	        zoom: 15,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	        mapTypeId: google.maps.MapTypeId.ROADMAP, 
+          zoomControl: false,
+          mapTypeControl: false,
+          streetViewControl:false,
+          draggable: false,
 	    };          
          
         var map = new google.maps.Map(document.getElementById("map-container"), mapOptions);          
          
+        var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
+
+        map.setOptions({styles: styles});
+
         $scope.map = map;   
         $ionicLoading.hide();  
 
         google.maps.event.addListenerOnce($scope.map, 'idle', function(){
          
+          var image = 'img/location_map.png';
           var marker = new google.maps.Marker({
               map: $scope.map,
               animation: google.maps.Animation.BOUNCE,
-              position: latlng
+              position: latlng,
+              icon: image
           });           
         });  
 
